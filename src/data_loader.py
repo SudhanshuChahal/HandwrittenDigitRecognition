e33b28fc331da40cdfb93a2d29de121bf0536563
SudhanshuChahal <sudhanshuchahal262@gmail.com>
2025-02-02 03:13:46 +0530

    Model creation, training and saving done.

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,9 +1,14 @@
 from tensorflow import keras
+import numpy as np
 
 def load_data():
 
-    (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data() # Load MNIST data from keras
+    (x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()   # Load MNIST data from keras
+
     x_train = x_train.astype('float32') / 255
-    x_test = x_test.astype('float32') / 255
+    x_test = x_test.astype('float32') / 255   # Normalize pixel values to be between 0 and 1
+
+    x_train = x_train[..., np.newaxis]
+    x_test = x_test[..., np.newaxis]   # Add a channel dimension
 
     return (x_train, y_train), (x_test, y_test)
--- src/train.py
+++ src/train.py
@@ -1,10 +1,18 @@
-from data_loader import load_data   
+from data_loader import load_data 
+from model import create_model # import cnn 
+import os  
 
 def train():
-
+    
     (x_train, y_train), (x_test, y_test) = load_data()
-    print("x_train shape:", x_train.shape)
-    print("x_test shape:", x_test.shape)
+    print('x_train shape:', x_train.shape)
+    print('x_test shape:', x_test.shape) # Load data
+
+    model = create_model() 
+    model.fit(x_train, y_train, epochs=5, validation_data=(x_test, y_test)) #  create and train model
+
+    model.save("../models/model.keras") # Save model
+
 
 
 if __name__ == '__main__':
